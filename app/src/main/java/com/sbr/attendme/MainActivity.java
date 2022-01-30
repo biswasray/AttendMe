8fe994ce470b9169fa9c30915f48098b017b7096
Subhasish Biswasray <subhasish9583672357@gmail.com>
2022-01-30 22:39:23 +0530

    Teacher & Student activities added and setup wifi manager

--- app/src/main/java/com/sbr/attendme/MainActivity.java
+++ app/src/main/java/com/sbr/attendme/MainActivity.java
@@ -2,10 +2,13 @@ package com.sbr.attendme;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -23,13 +26,16 @@ public class MainActivity extends AppCompatActivity implements CreateTeacher.Dis
         setContentView(R.layout.activity_main);
         MY_ID= Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
         db=new DBHelper(this);
+        checkPermission();
         if(db.tableExists(DBHelper.TEACHER_TABLE_NAME)) {
             Intent intent = new Intent(this, ListOfClasses.class);
             startActivity(intent);
             finish();
         }
         else if(db.tableExists(DBHelper.STUDENT_TABLE_NAME)) {
-            Toast.makeText(this,"student exist",Toast.LENGTH_SHORT).show();
+            Intent intent=new Intent(this,StuDiscActivity.class);
+            startActivity(intent);
+            finish();
         }
         ct=new CreateTeacher(db);
         ((Button)findViewById(R.id.createteacher)).setOnClickListener(new View.OnClickListener() {
@@ -47,6 +53,13 @@ public class MainActivity extends AppCompatActivity implements CreateTeacher.Dis
         });
     }
 
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+            checkPermission();
+        }
+    }
+
     @Override
     public void onDismiss(@NonNull DialogInterface dialog) {
 
@@ -56,6 +69,11 @@ public class MainActivity extends AppCompatActivity implements CreateTeacher.Dis
     public void onDialogPositiveClick(DialogFragment dialog,int i) {
         if(i==0) {
             Intent intent = new Intent(this, ListOfClasses.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(i==1) {
+            Intent intent=new Intent(this,StuDiscActivity.class);
             startActivity(intent);
             finish();
         }
---
+++ app/src/main/java/com/sbr/attendme/StuDiscActivity.java
@@ -0,0 +1,62 @@
+package com.sbr.attendme;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class StuDiscActivity extends AppCompatActivity {
+    private WifiManager manager;
+    private WifiP2pManager p2pmanager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stu_disc);
+        manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        p2pmanager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        //channel = manager.initialize(this, getMainLooper(), null);
+        check();
+    }
+    public void check() {
+        if(manager.isWifiEnabled()) {
+            proceed();
+        }
+        else {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q) {
+                Toast.makeText(this,"Please turn on WiFi",Toast.LENGTH_SHORT).show();
+                Intent panel=new Intent(Settings.Panel.ACTION_WIFI);
+                //startActivityForResult(panel,1);
+                wifiOpener.launch(panel);
+            }
+            else {
+                manager.setWifiEnabled(true);
+            }
+        }
+    }
+
+    ActivityResultLauncher<Intent> wifiOpener= registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == 0) {
+                        Intent data = result.getData();
+                        check();
+                    }
+                }
+            });
+
+    public void proceed() {
+        Toast.makeText(this,"Searching for class",Toast.LENGTH_SHORT).show();
+    }
+}
